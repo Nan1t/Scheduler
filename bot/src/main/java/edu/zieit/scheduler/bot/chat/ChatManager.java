c9d8541560046e1acfd8033ea4fa86a3c630de49
Nanit <nanitmix@gmail.com>
2021-12-20 14:00:55 +0200

    Threads fix

--- bot/src/main/java/edu/zieit/scheduler/bot/chat/ChatManager.java
+++ bot/src/main/java/edu/zieit/scheduler/bot/chat/ChatManager.java
@@ -3,7 +3,6 @@ package edu.zieit.scheduler.bot.chat;
 import edu.zieit.scheduler.bot.SchedulerBot;
 import edu.zieit.scheduler.bot.states.StateDenyAll;
 import edu.zieit.scheduler.bot.states.StateHelp;
-import edu.zieit.scheduler.bot.states.aud.StateAudComp;
 import edu.zieit.scheduler.bot.states.aud.StateAudList;
 import edu.zieit.scheduler.bot.states.aud.StateAudShow;
 import edu.zieit.scheduler.bot.states.consult.StateConsult;
@@ -28,6 +27,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -62,61 +62,63 @@ public class ChatManager {
     }
 
     public void handleUpdate(Update update) {
-        String chatId = getChatId(update);
+        CompletableFuture.runAsync(() -> {
+            String chatId = getChatId(update);
 
-        if (chatId == null) return;
+            if (chatId == null) return;
 
-        ChatSession session = getOrCreateSession(chatId);
-        ChatInput input = new ChatInput(chatId, session, update);
+            ChatSession session = getOrCreateSession(chatId);
+            ChatInput input = new ChatInput(chatId, session, update);
 
-        if (update.hasMessage()) {
-            session.resetLastMsgId();
+            if (update.hasMessage()) {
+                session.resetLastMsgId();
 
-            if (update.getMessage().isCommand()) {
-                String cmdRaw = update.getMessage().getText();
-                String[] arr = cmdRaw.split("@");
+                if (update.getMessage().isCommand()) {
+                    String cmdRaw = update.getMessage().getText();
+                    String[] arr = cmdRaw.split("@");
 
-                if (arr.length > 1 && !arr[1].equalsIgnoreCase(bot.getBotUsername())) {
-                    // Command not for this bot
-                    endSession(chatId);
+                    if (arr.length > 1 && !arr[1].equalsIgnoreCase(bot.getBotUsername())) {
+                        // Command not for this bot
+                        endSession(chatId);
+                        return;
+                    }
+
+                    String cmd = arr[0].substring(1);
+                    logger.info("Income command: '" + cmd + "' from " + getUser(update));
+
+                    State state = getBaseState(cmd);
+
+                    if (state != null) {
+                        session.updateState(state);
+
+                        if (!state.hasNext())
+                            endSession(chatId);
+                    } else {
+                        bot.sendMessage(session, bot.getLang().of("cmd.unsupported"));
+                    }
+                    return;
+                }
+            }
+
+            State state = session.getState();
+
+            if (state != null) {
+                InputResult result = state.input(input, session);
+
+                if (result.equals(InputResult.STAY))
+                    return;
+
+                if (result.equals(InputResult.NEXT)) {
+                    session.updateState(state.getNext());
                     return;
                 }
 
-                String cmd = arr[0].substring(1);
-                logger.info("Income command: '" + cmd + "' from " + getUser(update));
-
-                State state = getBaseState(cmd);
-
-                if (state != null) {
-                    session.updateState(state);
-
-                    if (!state.hasNext())
-                        endSession(chatId);
-                } else {
-                    bot.sendMessage(session, bot.getLang().of("cmd.unsupported"));
-                }
-                return;
-            }
-        }
-
-        State state = session.getState();
-
-        if (state != null) {
-            InputResult result = state.input(input, session);
-
-            if (result.equals(InputResult.STAY))
-                return;
-
-            if (result.equals(InputResult.NEXT)) {
-                session.updateState(state.getNext());
-                return;
+                if (!state.hasNext())
+                    endSession(chatId);
             }
 
-            if (!state.hasNext())
-                endSession(chatId);
-        }
-
-        bot.sendMessage(session, bot.getLang().of("cmd.unsupported"));
+            bot.sendMessage(session, bot.getLang().of("cmd.unsupported"));
+        }, threadPool);
     }
 
     public void shutdownThreads() {
